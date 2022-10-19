993adc15a4c709eacf9753240c3188f95116b1a4
Marc <zunly@hotmail.com>
2022-10-19 23:09:11 +0800

    undefined reference...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "another_file.h"
 
+extern "C" {
+#include "libavutil/log.h"
+#include "libavformat/avformat.h"
+}
+
 int main() {
-    std::cout << "Hello, World?" << std::endl;
-    handle();
+    int ret;
+    av_log_set_level(AV_LOG_DEBUG);
+    av_log(NULL, AV_LOG_INFO, "test\n");
+    avformat_network_init();
+    avformat_network_deinit();
     return 0;
 }
