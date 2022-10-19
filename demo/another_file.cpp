1e94e1a725030550bd854cf1e6095583bf2186a6
Marc <zunly@hotmail.com>
2022-10-19 22:40:51 +0800

    init

---
+++ demo/another_file.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+void handle(void) {
+    std::cout << "from handle" << std::endl;
+
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "another_file.h"
+
+int main() {
+    std::cout << "Hello, World?" << std::endl;
+    handle();
+    return 0;
+}
